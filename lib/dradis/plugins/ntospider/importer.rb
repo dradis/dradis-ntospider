0e1bfc9ce2a0b70eaf0c04888b3a932075eb16d9
Jose A. Rilla <jose@rilla.es>
2015-05-15 12:10:04 +0200

    Basic summary parsing and vuln recognition

--- lib/dradis/plugins/ntospider/importer.rb
+++ lib/dradis/plugins/ntospider/importer.rb
@@ -5,12 +5,24 @@ module Dradis::Plugins::NTOSpider
     # the dropdown list and uploads a file.
     # @returns true if the operation was successful, false otherwise
     def import(params={})
-      file_content    = File.read( params[:file] )
+      file_content = File.read( params[:file] )
 
-      logger.info{'Parsing NTOSpider report files...'}
+      logger.info{ 'Parsing NTOSpider report files...' }
+      doc = Nokogiri::XML( file_content )
+      logger.info{ 'Done.' }
 
-      logger.info{'Done.'}
+      if doc.root.name != 'VulnSummary'
+        error = "Document doesn't seem to be VulnerabilitiesSummary.xml file from NTO"
+        logger.fatal{ error }
+        content_service.create_note text: error
+        return false
+      end
 
+      doc.xpath('VulnSummary/VulnList/Vuln').each do |xml_vuln|
+        vuln_name = xml_vuln.at('VulnType').text
+        vuln_type = xml_vuln.at('AttackClass').text
+        logger.info{ "Adding #{ vuln_name } (#{ vuln_type })" }
+      end
       return true
     end # /import
 
--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,48 +1,44 @@
-module Dradis
-  module Plugins
-    module NTOSpider
+class NTOSpiderTasks < Thor
 
-      class Thor < Thor
-        include Core::Pro::ProjectScopedTask
-        namespace "dradis:plugins:ntospider"
+  class Thor < Thor
+    include Core::Pro::ProjectScopedTask
+    namespace "dradis:plugins:ntospider"
 
-        desc "upload FILE", "upload NTOSpider XML results"
-        def upload(file_path)
-          require 'config/environment'
+    desc "upload FILE", "upload NTOSpider VulnerabilitiesSummary.xml"
+    def upload(file_path)
+      require 'config/environment'
 
-          logger = Logger.new(STDOUT)
-          logger.level = Logger::DEBUG
+      logger = Logger.new(STDOUT)
+      logger.level = Logger::DEBUG
 
-          unless File.exists?(file_path)
-            $stderr.puts "** the file [#{file_path}] does not exist"
-            exit -1
-          end
-
-          detect_and_set_project_scope
-
-          content_service = nil
-          template_service = nil
-          if defined?(Dradis::Pro)
-            content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::NTOSpider)
-            template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::NTOSpider)
-          else
-            content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::NTOSpider)
-            template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::NTOSpider)
-          end
-
-          importer = Dradis::Plugins::NTOSpider::Importer.new(
-            logger: logger,
-            content_service: content_service,
-            template_service: template_service
-          )
-
-          importer.import(file: file_path)
-
-          logger.close
-        end
-
+      unless File.exists?(file_path)
+        $stderr.puts "** the file [#{file_path}] does not exist"
+        exit -1
       end
 
+
+      content_service = nil
+      template_service = nil
+      if defined?(Dradis::Pro)
+        detect_and_set_project_scope
+        content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::NTOSpider)
+        template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::NTOSpider)
+      else
+        content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::NTOSpider)
+        template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::NTOSpider)
+      end
+
+      importer = Dradis::Plugins::NTOSpider::Importer.new(
+        logger: logger,
+        content_service: content_service,
+        template_service: template_service
+      )
+
+      importer.import(file: file_path)
+
+      logger.close
     end
+
   end
+
 end
