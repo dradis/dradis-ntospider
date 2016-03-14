eb82d4e0720d1e7cc6742806bbc5e5871c8685b9
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:52:47 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/ntospider.rb
+++ lib/dradis/plugins/ntospider.rb
@@ -9,21 +9,3 @@ require 'dradis/plugins/ntospider/engine'
 require 'dradis/plugins/ntospider/field_processor'
 require 'dradis/plugins/ntospider/importer'
 require 'dradis/plugins/ntospider/version'
-
-
-module Dradis
-  module Plugins
-    module NTOSpider
-      # This is required while we transition the Upload Manager to use
-      # Dradis::Plugins only
-      module Meta
-        NAME = "NTOSpider report upload plugin"
-        EXPECTS = "NTOSpider report."
-        module VERSION
-          include Dradis::Plugins::NTOSpider::VERSION
-        end
-      end
-    end
-  end
-end
-
