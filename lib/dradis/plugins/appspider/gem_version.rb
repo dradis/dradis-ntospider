d042eec9e4e806841b50bfe46b1904e8e21fbf32
Michael Gargiullo <mgargiullo@pvtpt.com>
2019-09-04 14:31:09 -0400

    Updated gem_version.

--- lib/dradis/plugins/appspider/gem_version.rb
+++ lib/dradis/plugins/appspider/gem_version.rb
@@ -10,7 +10,7 @@ module Dradis
         MAJOR = 3
         MINOR = 15
         TINY = 0
-        PRE = rc1
+        PRE = 'rc1'
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
       end
