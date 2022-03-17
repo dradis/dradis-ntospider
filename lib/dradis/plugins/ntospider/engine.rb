e53c41603d2d3abd1e7ca5f3bdc7a7a2ef94a37a
Brian Pearce <brian.o.pearce@gmail.com>
2022-03-17 15:21:20 +0100

    Register templates locally

--- lib/dradis/plugins/ntospider/engine.rb
+++ lib/dradis/plugins/ntospider/engine.rb
@@ -5,5 +5,9 @@ module Dradis::Plugins::NTOSpider
     include ::Dradis::Plugins::Base
     description 'Processes NTOSpider reports'
     provides :upload
+
+    def self.template_names
+      { module_parent => { evidence: 'evidence', issue: 'vuln' } }
+    end
   end
 end
