1db5531d6da560809c577d972ed13ea2462db41c
Brian Pearce <brian.o.pearce@gmail.com>
2022-04-08 18:32:58 +0200

    Move the template list to the importer

--- lib/dradis/plugins/ntospider/engine.rb
+++ lib/dradis/plugins/ntospider/engine.rb
@@ -5,9 +5,5 @@ module Dradis::Plugins::NTOSpider
     include ::Dradis::Plugins::Base
     description 'Processes NTOSpider reports'
     provides :upload
-
-    def self.template_names
-      { module_parent => { evidence: 'evidence', issue: 'vuln' } }
-    end
   end
 end
